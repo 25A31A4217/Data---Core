4da84af69d131db0a11ea32dda620a5cdbd761d1
Devisri Natte <devisrinatte4787@gmail.com>
2026-04-09 14:38:19 +0530

    Add files via upload

---
+++ devi 22.cpp
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+// Simple stack example
+#define MAX 5
+
+int stack[MAX];
+int top = -1;
+
+void push(int value) {
+    if (top == MAX - 1) {
+        printf("Stack Overflow\n");
+    } else {
+        stack[++top] = value;
+    }
+}
+
+void pop() {
+    if (top == -1) {
+        printf("Stack Underflow\n");
+    } else {
+        printf("Popped: %d\n", stack[top--]);
+    }
+}
+
+void display() {
+    for (int i = top; i >= 0; i--) {
+        printf("%d ", stack[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    push(10);
+    push(20);
+    push(30);
+
+    display();
+    pop();
+    display();
+
+    return 0;
+}
